75ff6d007b90f9c0f3df6a2b802f5e33a9ab2b08
hollowblind095 <hollowblind095@gmail.com>
2022-03-27 18:12:00 -0300

    tailwind react template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      {/* Acá irían sus otros componentes */}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-sky-500 py-12 text-center">Footer</footer>
+  )
+}
+
+export default Footer
