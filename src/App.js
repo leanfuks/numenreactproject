75e7bcccbab47f888ee065c1186d9bdc71f0be8c
Jonatan Jofre <jonajofre89@hotmail.com>
2022-04-10 17:32:48 -0300

    carrusel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
+import Carrusel from "./components/Carrusel";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Carrusel/> 
       <Footer />
     </div>
   );
