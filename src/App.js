55038d4a0317e86b66339408678c4b4662284845
Lean Fuks <leandro_fuks@yahoo.com>
2022-04-19 00:47:16 -0300

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React from 'react'
-import NavBar from '..components/1NavBar'
-import Banner from '..components/2Banner'
-import LatestOffers from '..components/3LatestOffers'
-import WhyChoose from '..components/4WhyChoose'
-import WhoMakes from '..components/5WhoMakes'
-import OurPartners from '..components/6OurPartners'
-import SurpriseSomeone from '..components/7SurpriseSomeone'
-import Carousel from '..components/8Carousel'
-import TellAFriend from '..components/9TellAFriend'
-import Footer from '..components/10Footer'
-import Extra from '..components/Extra'
+import NavBar from "./components/1NavBar";
+import Banner from './components/2Banner';
+import LatestOffers from './components/3LatestOffers'
+import WhyChoose from './components/4WhyChoose'
+import WhoMakes from './components/5WhoMakes'
+import OurPartners from './components/6OurPartners'
+import SurpriseSomeone from './components/7SurpriseSomeone'
+import Carousel from './components/8Carousel'
+import TellAFriend from './components/9TellAFriend'
+import Footer from "./components/10Footer"
+
 
 const App = () => {
   return (
     <div>
-      <NavBar />
+      
+      <NavBar/>
       <Banner />
-      <LatestOffers />
-      <WhyChoose />
-      <WhoMakes />
-      <OurPartners />
-      <SurpriseSomeone />
-      <Carousel />
-      <TellAFriend />
-      <Footer />
-      <Extra />
+      <LatestOffers/>
+      <WhyChoose/>
+      <WhoMakes/>
+      <OurPartners/>
+      <SurpriseSomeone/>
+      <Carousel/>
+      <TellAFriend/>
+      <Footer/>
+      
+      
+     
+    
+      
+      
+      
     </div>
   )
 }
