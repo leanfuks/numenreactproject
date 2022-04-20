a895d4081e54ce41237ff87f7581ac3fe7fe6ef2
Lean Fuks <leandro_fuks@yahoo.com>
2022-04-20 12:21:29 -0300

    Cambios estilos cajas de ultimas ofertas

--- src/components/3LatestOffers/index.jsx
+++ src/components/3LatestOffers/index.jsx
@@ -9,27 +9,28 @@ const latestoffers = () => {
   return (
     <>
       <p className="p-2 font-calibri text-xl text-center bg-blue-100">
-        <strong>¡ULTIMAS OFERTAS!</strong>
+        <strong>¡ÚLTIMAS OFERTAS!</strong>
       </p>
-      <div id="ofertas" className="flex flex-wrap justify-center w-full p-0 m-0 font-calibri bg-blue-200">
+      <div id="ofertas" className="flex flex-wrap justify-center items-center w-full p-0 m-0 font-calibri bg-blue-100">
         
         
-        <ul className="m-4 font-calibri bg-blue-500 rounded-lg rounded-t-xl item-center w-80 h-80">
+        <ul className="m-4 font-calibri bg-blue-200 rounded-lg rounded-t-xl item-center w-80 h-80">
           <li>
             <img
               src={tv}
               alt="imagenTV"
-              className="h-40 p-2 bg-blue-500 rounded-lg w-80 "
+              className="h-40 p-2 rounded-lg w-80 "
             />
           </li>
-          <li className="m-2 text-center bg-blue-200 rounded-lg">
-            Smart tv 150" TCL
+          <li className="m-2 font-bold text-center ">
+            Smart TV 4K 150" TCL
           </li>
-          <li className="m-2 text-center bg-blue-300 rounded-lg">$290.000</li>
-          <li className="m-2 text-center line-through bg-blue-400 rounded-lg">
-            $390.000
+          <li className=" flex flex-row justify-center items-center font-semibold text-center text-red-500">SALE! 290.000
+          <li className=" ml-2 text-sm font-normal text-black line-through ">
+            $390.000</li>
+          
           </li>
-          <li className="m-2 text-center bg-blue-200 rounded-lg">
+          <li className="m-2 text-center bg-blue-300 rounded-lg">
             <button className="rounded sm:ease-in-out sm:delay-80 sm:hover:-translate-y-1 sm:hover:scale-110 sm:hover:bg-blend-hard-light sm:duration-300">
               <a target="blank" src={Tele}>mas info aqui</a>
             </button>
