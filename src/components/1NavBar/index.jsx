b3f8d1a8bafcff9e7cd33a878512b433b787c045
Ivonne Tournier <tournierivon@gmail.com>
2022-05-14 23:04:00 -0300

    se añade link al navbar

--- src/components/1NavBar/index.jsx
+++ src/components/1NavBar/index.jsx
@@ -1,44 +1,56 @@
 import { navigationBar } from "../../data";
-import logo from "../Imagenes/logo.png"
-import search from "../Imagenes/search.png"
-import shop from "../Imagenes/shop.png"
-import user from "../Imagenes/user.png"
+import logo from "../Imagenes/logo.png";
+import search from "../Imagenes/search.png";
+import shop from "../Imagenes/shop.png";
+import user from "../Imagenes/user.png";
 
 const NavBar = () => {
+  return (
 
+    <div className="flex flex-wrap items-center justify-center w-full gap-12 p-4 text-lg text-black bg-blue-200 xsm:flex-col sm:flex-row font-calibri">
+      
+        <img className="h-20 p-1 w-52" src={logo} alt="NumenStore" />
+      
 
-
-
-return (
-  <div className=" p-4 flex  xsm:flex-col sm:flex-row flex-wrap justify-center items-center gap-12 text-lg font-calibri w-full  text-black bg-blue-200">
-
-    <a href="www.numenstore.com"><img className=" h-20 p-1 w-52 " src={logo} alt="NumenStore" /></a>
-
-    
       {navigationBar.map((seccion) => (
-        <nav key={seccion.id}>
-        <ul className=" flex delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light duration-300  list-none items-center justify-center ">
-          <li>
-            <button> {seccion.button} </button>
-          </li>
-        </ul>
-        </nav >
-      )
-      )}
-    
-    
+        <nav key={seccion.id}><a href="www.numenstore.com"target="blank">
+          <ul className="flex items-center justify-center list-none duration-300 delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light">
+            <li>
+              <button> {seccion.button}  </button>
+            </li>
+          </ul></a>
+        </nav>
+      ))}
       <ul className="list-none space-between">
-        <li className=" ">
-          <button> <img className="ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light duration-300 w-6 h-6 mx-1" src={search} alt="search" /> </button>
-          <button> <img className="ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light duration-300 w-6 h-6 mx-1" src={shop} alt="carrito" /></button>
-          <button> <img className="ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light duration-300 w-6 h-6 mx-1" src={user} alt="usuario" /></button>
-          
+        <li className="">
+          <button>
+            {" "}
+            <img
+              className="w-6 h-6 mx-1 duration-300 ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light"
+              src={search}
+              alt="search"
+            />{" "}
+          </button>
+          <button>
+            {" "}
+            <img
+              className="w-6 h-6 mx-1 duration-300 ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light"
+              src={shop}
+              alt="carrito"
+            />
+          </button>
+          <button>
+            {" "}
+            <img
+              className="w-6 h-6 mx-1 duration-300 ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-blend-hard-light"
+              src={user}
+              alt="usuario"
+            />
+          </button>
         </li>
       </ul>
-    
-    
-  </div>
-)
+    </div>
+  );
 };
 
-export default NavBar
+export default NavBar;
